c590d555006b028b20e65db64d3d2a02a8c6130e
swarn007-byte <swarnshekhar007@gmail.com>
2026-06-07 23:01:07 +0530

    dn

---
+++ A_Euclid_Sequence_and_Two_Numbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <functional>
+
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> b(n);
+        for(auto& x : b) cin >> x;
+        
+        sort(b.begin(), b.end(), greater<int>());
+        
+        bool ok = true;
+        for(int i = 0; i + 2 < n; i++) {
+            if(b[i] % b[i+1] != b[i+2]) {
+                ok = false;
+                break;
+            }
+        }
+        
+        if(ok) cout << b[0] << " " << b[1] << "\n";
+        else cout << -1 << "\n";
+    }
+}
---
+++ C_Contrast_Value.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+int main()
+{   
+    ios;
+    #ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+    freopen("/Users/sreejith/Desktop/output.txt", "w", stdout);
+    #endif
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n,b,c=0,state=0;
+        cin>>n;
+        vector < ll > v;
+        for(ll i = 0 ; i < n ; i++) {
+            cin>>b;
+            v.push_back(b);
+        }
+        if(n==1) {
+            cout<<1<<endl;
+            continue;
+        }
+        for(ll i = 1 ; i < n ; i++) {
+            if((v[i-1] > v[i]) and state!=1){
+                c++;
+                state = 1;
+            }
+            else if((v[i-1] < v[i]) and state!=-1) {
+                c++;
+                state = -1;
+            }
+        }
+        cout<<c+1<<endl;
+    }
+    return 0;     
+ }        
